173e506c645215d0f7b18d5a4893244ddd5b65e1
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2019-01-20 20:54:40 +0530

    First commit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('Before')
-console.log('After')
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+const genres = require('./routes/genres');
+
+app.use(express.json());
+app.use('/api/genres/', genres);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
